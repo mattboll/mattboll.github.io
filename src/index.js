286e63f537429c36a514de3c9a6ed9b709c41f2e
Matthieu Bollot <m.bollot@code-troopers.com>
2024-03-04 10:32:26 +0100

    Reinit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// JS Goes here - ES6 supported
+
+import "./css/main.scss";
+import "../node_modules/lightbox2/dist/js/lightbox-plus-jquery";
+
+
+const hljs = require("../node_modules/highlight.js/lib/common");
+hljs.highlightAll();
